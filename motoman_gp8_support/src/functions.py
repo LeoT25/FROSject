618db927c640143f00d6d18b8d4c4fa6e108b4b8
Leonardo Tarazona <54273599+LeoT25@users.noreply.github.com>
2023-11-22 09:36:51 -0500

    Update functions.py

--- motoman_gp8_support/src/functions.py
+++ motoman_gp8_support/src/functions.py
@@ -70,70 +70,52 @@ def jacobian_position(q, delta=0.0001):
     return J
 
 
-def jacobian_pose(q, delta=0.0001):
-    """
-    Jacobiano analitico para la posicion y orientacion (usando un
-    cuaternion). Retorna una matriz de 7x6 y toma como entrada el vector de
-    configuracion articular q=[q1, q2, q3, q4, q5, q6]
+def ikine_mmgp8(xdes, q0):
+   """
+   Calcular la cinematica inversa de UR5 numericamente a partir de la configuracion articular inicial de q0.
+   Emplear el metodo de newton
+   """
+   epsilon  = 0.001
+   max_iter = 1000
+   delta    = 0.00001
+   # Files for the logs
+   error = open("/home/user/lab_ws/errorac.txt", "w")    
 
-    """
-    J = np.zeros((7,6))
-    # Implementar este Jacobiano aqui
+   q  = copy(q0)
+   for i in range(max_iter):
+       # Main loop
+       J = jacobian_position(q)
+       f = fkine_mmgp8(q)
+       e = xdes - f[0:3,3]
+       q = q + np.dot(J.T, e)
+       error.write(str(e[0])+' '+str(e[1]) +' '+str(e[2])+'\n')
+       if (np.linalg.norm(e) < epsilon):
+           break
+       pass
+   return q
 
-    
-    return J
-
-
-
-def rot2quat(R):
-    """
-    Convertir una matriz de rotacion en un cuaternion
-
-    Entrada:
-      R -- Matriz de rotacion
-    Salida:
-      Q -- Cuaternion [ew, ex, ey, ez]
-
-    """
-    dEpsilon = 1e-6
-    quat = 4*[0.,]
-
-    quat[0] = 0.5*np.sqrt(R[0,0]+R[1,1]+R[2,2]+1.0)
-    if ( np.fabs(R[0,0]-R[1,1]-R[2,2]+1.0) < dEpsilon ):
-        quat[1] = 0.0
-    else:
-        quat[1] = 0.5*np.sign(R[2,1]-R[1,2])*np.sqrt(R[0,0]-R[1,1]-R[2,2]+1.0)
-    if ( np.fabs(R[1,1]-R[2,2]-R[0,0]+1.0) < dEpsilon ):
-        quat[2] = 0.0
-    else:
-        quat[2] = 0.5*np.sign(R[0,2]-R[2,0])*np.sqrt(R[1,1]-R[2,2]-R[0,0]+1.0)
-    if ( np.fabs(R[2,2]-R[0,0]-R[1,1]+1.0) < dEpsilon ):
-        quat[3] = 0.0
-    else:
-        quat[3] = 0.5*np.sign(R[1,0]-R[0,1])*np.sqrt(R[2,2]-R[0,0]-R[1,1]+1.0)
-
-    return np.array(quat)
+def ik_gradient_mmgp8(xdes, q0):
+   """
+   Calcular la cinematica inversa de UR5 numericamente a partir de la configuracion articular inicial de q0.
+   Emplear el metodo gradiente
+   """
+   epsilon  = 0.001
+   max_iter = 1000
+   delta    = 0.001
+   alpha = 0.1
 
 
-def TF2xyzquat(T):
-    """
-    Convert a homogeneous transformation matrix into the a vector containing the
-    pose of the robot.
-
-    Input:
-      T -- A homogeneous transformation
-    Output:
-      X -- A pose vector in the format [x y z ew ex ey ez], donde la first part
-           is Cartesian coordinates and the last part is a quaternion
-    """
-    quat = rot2quat(T[0:3,0:3])
-    res = [T[0,3], T[1,3], T[2,3], quat[0], quat[1], quat[2], quat[3]]
-    return np.array(res)
-
-
-def skew(w):
-    R = np.zeros([3,3])
-    R[0,1] = -w[2]; R[0,2] = w[1]
-    R[1,0] = w[2];  R[1,2] = -w[0]
-    R[2,0] = -w[1]; R[2,1] = w[0]
-    return R
+   q  = copy(q0)
+   for i in range(max_iter):
+       # Main loop
+       J = jacobian_position(q)
+       f = fkine_mmgp8(q)
+       e = xdes - f[0:3,3]
+       q = q + alpha*np.dot(J.T, e)
+      
+       if (np.linalg.norm(e) < epsilon):
+           break
+          
+       pass
+  
+   return q
